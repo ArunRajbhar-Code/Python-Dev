d73d8eaf7c2d710a8d4156fa717388d30784bbba
Arun4Rajbhar <aruanamika7275@gmail.com>
2024-11-13 21:17:28 +0530

    todo ini-1

--- python-programming/Basics/todo1.py
+++ python-programming/Basics/todo1.py
@@ -4,24 +4,27 @@ while True:
 1 Type 'add' for adding to dos
 2 Type 'show' for showing all the to dos
 3 Type 'exit' for close the program 
-4 Type 'edit' for editing the to do                                                                
+4 Type 'edit' for editing the to do    
+5 Type 'complete' for completing the to do                                                                                  
 """)
-    match user_Action.strip():
+    match user_Action.strip():# strip function is used to remove spaces 
         case "add":
             todo=input("Enter the todo ")
             todos.append(todo.capitalize())
         case "show":
             print("List of task to do".upper())
-            for i in todos:
-                print("*",i)
+            for index,i in enumerate(todos):
+                print(index+1,"-",i)
         case "edit":
             print("List of task to do".upper())
-            for i in todos:
-                print("*",i)
+            for index,i in enumerate(todos):
+                print(index+1,"-",i)
             number=int(input("Please enter the no of to do to edit :"))
             todo=input(f"current value of to do is '{todos[number-1]}' Please enter new to do :")
             todos[number-1]=todo
-
+        case "complete":
+            number=int(input("Please enter the no of to do to complete :"))
+            todos.pop(number-1)
         case "exit":
             break    
         case _:
